4114f705866d27583dabcf9435c2f9a3f7151bd9
IasonC <iasonchaimalas@gmail.com>
2024-01-10 21:28:25 +0000

    formatting with black

--- AMLS_23_24_SN20018178/B/taskB.py
+++ AMLS_23_24_SN20018178/B/taskB.py
@@ -92,21 +92,21 @@ class SVM_Path(Data_Path):
     def set_classifier(self, ret: bool = False) -> None:
         """Set SVM hyperparameters"""
 
-        #classifier = svm.SVC(
+        # classifier = svm.SVC(
         #    C=self.C,
         #    gamma=self.gamma,
         #    kernel="linear",
         #    class_weight=None,
         #    decision_function_shape=self.multiclass,
         #    break_ties=self.break_ties,
-        #)
+        # )
 
         classifier = svm.LinearSVC(
-            penalty='l2',
-            loss='squared_hinge',
+            penalty="l2",
+            loss="squared_hinge",
             dual=False,
             C=self.C,
-            multi_class='ovr',
+            multi_class="ovr",
             verbose=1,
         )
 
@@ -117,8 +117,8 @@ class SVM_Path(Data_Path):
 
     def fit_classifier(self) -> None:
         """Fit SVM classifier to flattened data"""
-        
-        '''self.n_classes = np.unique(self.train_labels)
+
+        """self.n_classes = np.unique(self.train_labels)
 
         self.ovr_classifiers = []
         for c in tqdm(self.n_classes, desc='Fit OvR SVMs'):
@@ -130,8 +130,7 @@ class SVM_Path(Data_Path):
             classifier = self.set_classifier(ret=True)
             classifier.fit(self.svm_tr, labels)
 
-            self.ovr_classifiers.append(classifier)'''
-        
+            self.ovr_classifiers.append(classifier)"""
 
         self.classifier.fit(self.svm_tr, self.train_labels)
 
@@ -147,12 +146,12 @@ class SVM_Path(Data_Path):
         elif mode == "test":
             (x, y) = (self.svm_te, self.test_labels)
 
-        '''preds = np.zeros((len(y), self.n_classes))
+        """preds = np.zeros((len(y), self.n_classes))
         for c in tqdm(range(len(self.ovr_classifiers)), desc='Predict from OvR SVMs'):
             classifier = self.ovr_classifiers[c]
             pred_c = classifier.predict(x)
             preds[pred_c == -1] += 1 #####################
-            preds[pred_c == c] += 1 #####################'''
+            preds[pred_c == c] += 1 #####################"""
 
         pred_labels = self.classifier.predict(x)
         accuracy = accuracy_score(y, pred_labels)
@@ -167,7 +166,7 @@ class SVM_Path(Data_Path):
             plt.savefig(f"B_svm_cm_{mode}.pdf")
 
         return accuracy
-    
+
 
 ############################################################################
 ############                                                    ############
@@ -178,19 +177,22 @@ class SVM_Path(Data_Path):
 import torch
 import torchvision
 from torchvision.models.resnet import ResNet18_Weights
-from torch.utils.data import TensorDataset, DataLoader 
+from torch.utils.data import TensorDataset, DataLoader
 from torchmetrics import Precision, Recall
-#from senet.se_resnet import resnet_model
+
+# from senet.se_resnet import resnet_model
 import copy
 
+
 class SqueezeExcitationResNet(Data_Path):
-    def __init__(self, 
-                 num_classes: int = 9, 
-                 batch_size: int = 64, 
-                 feature_extract: bool = True, 
-                 use_pretrained: bool = True, 
-                 device: str = 'cuda'
-                 ) -> None:
+    def __init__(
+        self,
+        num_classes: int = 9,
+        batch_size: int = 64,
+        feature_extract: bool = True,
+        use_pretrained: bool = True,
+        device: str = "cuda",
+    ) -> None:
         super().__init__()
 
         self.device = device
@@ -198,8 +200,8 @@ class SqueezeExcitationResNet(Data_Path):
         self.num_classes = num_classes
         self.dataloading()
 
-        #self.model = torchvision.models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1) #resnet_model()
-            # resnet pretrained on ImageNet (transfer learning)
+        # self.model = torchvision.models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1) #resnet_model()
+        # resnet pretrained on ImageNet (transfer learning)
         self.model_ft = torchvision.models.resnet18(pretrained=use_pretrained)
         self.set_parameter_requires_grad(self.model_ft, feature_extract)
         num_ftrs = self.model_ft.fc.in_features
@@ -207,27 +209,31 @@ class SqueezeExcitationResNet(Data_Path):
         self.input_size = 28
 
         self.model_ft.to(device)
-        
-        print('\nResNet18 Model:\n---------------------------------')
+
+        print("\nResNet18 Model:\n---------------------------------")
         print(self.model_ft)
 
         # Gather parameters for learning (based on pretraining & freezing)
         if feature_extract:
             params_to_update = []
-            for name,param in self.model_ft.named_parameters():
+            for name, param in self.model_ft.named_parameters():
                 if param.requires_grad == True:
                     params_to_update.append(param)
         else:
             params_to_update = self.model_ft.parameters()
-        
+
         self.optimizer = torch.optim.SGD(params_to_update, lr=0.001, momentum=0.9)
         self.criterion = torch.nn.CrossEntropyLoss()
-        #self.scheduler = lr_scheduler.StepLR(80, 0.1)
+        # self.scheduler = lr_scheduler.StepLR(80, 0.1)
 
         # metrics Precision & Recall
-        self.P = Precision(task="multiclass", average='macro', num_classes=num_classes).to(device)
-        self.R = Recall(task="multiclass", average='macro', num_classes=num_classes).to(device)
-            # macro averaging to emphasize class performance, not instance (avoid class imbalance)
+        self.P = Precision(
+            task="multiclass", average="macro", num_classes=num_classes
+        ).to(device)
+        self.R = Recall(task="multiclass", average="macro", num_classes=num_classes).to(
+            device
+        )
+        # macro averaging to emphasize class performance, not instance (avoid class imbalance)
 
     def set_parameter_requires_grad(self, model, feature_extracting):
         if feature_extracting:
@@ -235,7 +241,7 @@ class SqueezeExcitationResNet(Data_Path):
                 param.requires_grad = False
 
     def dataloading(self) -> None:
-        print('Dataloading starting...')
+        print("Dataloading starting...")
 
         tr = torch.Tensor(self.train).to(self.device)
         tr_label = torch.tensor(self.train_labels, dtype=torch.long).to(self.device)
@@ -248,7 +254,9 @@ class SqueezeExcitationResNet(Data_Path):
         val_dataset = TensorDataset(v, v_label)
         test_dataset = TensorDataset(te, te_label)
 
-        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        train_dataloader = DataLoader(
+            train_dataset, batch_size=self.batch_size, shuffle=True
+        )
         val_dataloader = DataLoader(val_dataset, batch_size=self.batch_size)
         test_dataloader = DataLoader(test_dataset, batch_size=self.batch_size)
 
@@ -258,37 +266,42 @@ class SqueezeExcitationResNet(Data_Path):
             test=test_dataloader,
         )
 
-        print('Dataloading done...')
+        print("Dataloading done...")
 
     def train_model(self, epochs):
-        
-        '''tqdm_rep = reporters.TQDMReporter(range(epochs))
+        """tqdm_rep = reporters.TQDMReporter(range(epochs))
         tensorboard_rep = reporters.TensorboardReporter('./senet/logdir/')
         _callbacks = [tqdm_rep, tensorboard_rep, callbacks.AccuracyCallback()]
         with Trainer(self.model, self.optimizer, F.cross_entropy, scheduler=self.scheduler, callbacks=_callbacks) as trainer:
             for _ in tqdm_rep:
                 trainer.train(self.train_dataloader)
-                trainer.test(self.val_dataloader)'''
-        
+                trainer.test(self.val_dataloader)"""
 
-        val_acc = [] # track val accuracy
+        val_acc = []  # track val accuracy
         self.best_model = copy.deepcopy(self.model_ft.state_dict())
         best_acc = 0.0
         count_not_better = 0
 
-        log = {'train-loss': [], 'train-acc': [], 'train-precision': [], 'train-recall': [],
-               'val-loss': [], 'val-acc': [], 'val-precision': [], 'val-recall': []}
+        log = {
+            "train-loss": [],
+            "train-acc": [],
+            "train-precision": [],
+            "train-recall": [],
+            "val-loss": [],
+            "val-acc": [],
+            "val-precision": [],
+            "val-recall": [],
+        }
 
         for e in range(epochs):
-
-            print(f'\nEPOCH {e} of {epochs}\n')
+            print(f"\nEPOCH {e} of {epochs}\n")
 
             # handle training and validation modes
-            for mode in ['train','val']:
-                if mode=='train':
+            for mode in ["train", "val"]:
+                if mode == "train":
                     self.model_ft.train()
-                elif mode=='val':
-                    self.model_ft.eval() # set mode
+                elif mode == "val":
+                    self.model_ft.eval()  # set mode
 
                 current_loss = 0.0
                 current_corrects = 0
@@ -297,29 +310,32 @@ class SqueezeExcitationResNet(Data_Path):
                 batch_count = 0
 
                 for x, y in self.dataloaders_dict[mode]:
-                    #x = x.to(self.device)
+                    # x = x.to(self.device)
 
                     if x.shape[0] < self.batch_size:
-                        continue # last batch, uneven
-                    
-                    #print(f'SHAPE: x = {x.shape}, y = {y.shape}')
+                        continue  # last batch, uneven
+
+                    # print(f'SHAPE: x = {x.shape}, y = {y.shape}')
                     x = torch.reshape(x, (self.batch_size, 3, 28, 28))
-                    #y = y.to(self.device)
-                
+                    # y = y.to(self.device)
+
                     self.optimizer.zero_grad()
 
-                    with torch.set_grad_enabled(mode=='train'):
+                    with torch.set_grad_enabled(mode == "train"):
                         y_pred = self.model_ft(x)
                         loss = self.criterion(y_pred, y)
-            
+
                         _, preds = torch.max(y_pred, 1)
-                        
+
                         (precision, recall) = (self.P(preds, y), self.R(preds, y))
-                            # macro average for emphasis on classes, not instances (counter class imbalance)
-                        precision, recall = precision.item(), recall.item() # extract value from 1-d torch.tensor output
+                        # macro average for emphasis on classes, not instances (counter class imbalance)
+                        precision, recall = (
+                            precision.item(),
+                            recall.item(),
+                        )  # extract value from 1-d torch.tensor output
 
                         # backward + optimize only if in training phase
-                        if mode == 'train':
+                        if mode == "train":
                             loss.backward()
                             self.optimizer.step()
 
@@ -332,39 +348,47 @@ class SqueezeExcitationResNet(Data_Path):
                     batch_count += 1
 
                 epoch_loss = current_loss / len(self.dataloaders_dict[mode].dataset)
-                epoch_acc = current_corrects.double() / len(self.dataloaders_dict[mode].dataset)
+                epoch_acc = current_corrects.double() / len(
+                    self.dataloaders_dict[mode].dataset
+                )
                 epoch_precision = current_precision / batch_count
                 epoch_recall = current_recall / batch_count
 
                 # add to log
-                log[f'{mode}-loss'].append(epoch_loss)
-                log[f'{mode}-acc'].append(epoch_acc)
-                log[f'{mode}-precision'].append(epoch_precision)
-                log[f'{mode}-recall'].append(epoch_recall)
+                log[f"{mode}-loss"].append(epoch_loss)
+                log[f"{mode}-acc"].append(epoch_acc)
+                log[f"{mode}-precision"].append(epoch_precision)
+                log[f"{mode}-recall"].append(epoch_recall)
 
-                print('{} Loss: {:.4f} Acc: {:.4f} Prec: {:.4f} Rec: {:.4f}'.format(mode, epoch_loss, epoch_acc, epoch_precision, epoch_recall))
+                print(
+                    "{} Loss: {:.4f} Acc: {:.4f} Prec: {:.4f} Rec: {:.4f}".format(
+                        mode, epoch_loss, epoch_acc, epoch_precision, epoch_recall
+                    )
+                )
 
                 # log val_acc and deep copy the model again if it is an improvement
-                if mode == 'val':
+                if mode == "val":
                     val_acc.append(epoch_acc)
                     if epoch_acc > best_acc:
                         best_acc = epoch_acc
                         self.best_model = copy.deepcopy(self.model_ft.state_dict())
-                    elif epoch_acc < 1.1 * best_acc: # next epoch less than 10 percent val-acc improvement
+                    elif (
+                        epoch_acc < 1.1 * best_acc
+                    ):  # next epoch less than 10 percent val-acc improvement
                         count_not_better += 1
 
-            print('=========================================')
-                
+            print("=========================================")
+
             # early stopping
             if count_not_better >= 10:
-                print('Early Stopping: 10 epochs with no val-acc improvement...')
+                print("Early Stopping: 10 epochs with no val-acc improvement...")
                 break
-        
+
         # save json log
-        with open('./logs/log_resnet_v1.json', 'w') as fp:
+        with open("./logs/log_resnet_v1.json", "w") as fp:
             json.dump(log, fp)
 
-        '''
+        """
         # Training
         for batch_idx, (x,y) in enumerate(self.train_dataloader):
             x = torch.reshape(x, (64,3,28,28))
@@ -384,7 +408,7 @@ class SqueezeExcitationResNet(Data_Path):
                 loss = self.criterion(ypred,y)
             
         print(f'Val Loss: {loss.item()}')
-        '''
+        """
 
     def test_model(self):
         # load best model
@@ -392,27 +416,30 @@ class SqueezeExcitationResNet(Data_Path):
 
         L, acc, prec, rec, batch_count = 0.0, 0, 0.0, 0.0, 0
 
-        for x, y in self.dataloaders_dict['test']:
+        for x, y in self.dataloaders_dict["test"]:
             if x.shape[0] < self.batch_size:
                 s = x.shape[0]
             else:
                 s = self.batch_size
-            
-            #print(f'SHAPE: x = {x.shape}, y = {y.shape}')
+
+            # print(f'SHAPE: x = {x.shape}, y = {y.shape}')
             x = torch.reshape(x, (s, 3, 28, 28))
-            #y = y.to(self.device)
-        
+            # y = y.to(self.device)
+
             self.optimizer.zero_grad()
 
             with torch.no_grad():
                 y_pred = self.model_ft(x)
                 loss = self.criterion(y_pred, y)
-    
+
                 _, preds = torch.max(y_pred, 1)
-                
+
                 (precision, recall) = (self.P(preds, y), self.R(preds, y))
-                    # macro average for emphasis on classes, not instances (counter class imbalance)
-                precision, recall = precision.item(), recall.item() # extract value from 1-d torch.tensor output
+                # macro average for emphasis on classes, not instances (counter class imbalance)
+                precision, recall = (
+                    precision.item(),
+                    recall.item(),
+                )  # extract value from 1-d torch.tensor output
 
             # statistics
             L += loss.item() * x.size(0)
@@ -421,16 +448,20 @@ class SqueezeExcitationResNet(Data_Path):
             rec += recall
             batch_count += 1
 
-        L /= len(self.dataloaders_dict['test'].dataset)
-        acc = acc.double() / len(self.dataloaders_dict['test'].dataset)
+        L /= len(self.dataloaders_dict["test"].dataset)
+        acc = acc.double() / len(self.dataloaders_dict["test"].dataset)
         prec /= batch_count
         rec /= batch_count
 
-        print('TEST DATA results:\nLoss {:.4f} || Acc {:.4f} || Precision {:.4f} || Recall {:.4f}'.format(L, acc, prec, rec))
+        print(
+            "TEST DATA results:\nLoss {:.4f} || Acc {:.4f} || Precision {:.4f} || Recall {:.4f}".format(
+                L, acc, prec, rec
+            )
+        )
 
 
 if __name__ == "__main__":
-    '''s = SVM_Path()
+    """s = SVM_Path()
     s.set_classifier()
     tf_i = time.time()
     s.fit_classifier()
@@ -443,7 +474,7 @@ if __name__ == "__main__":
     tp_te = time.time()
 
     print(f'\nTime to fit: {tf_f - tf_i}')
-    print(f'Time to pred: {tp_tr-tf_f} Train | {tp_v-tp_tr} Val | {tp_te-tp_v} Test')'''
+    print(f'Time to pred: {tp_tr-tf_f} Train | {tp_v-tp_tr} Val | {tp_te-tp_v} Test')"""
 
     se = SqueezeExcitationResNet(feature_extract=False)
     se.train_model(epochs=100)
